f9f2608c0d215a34de333b2b37a6203b864d4878
hectorrami <hramirez4@uh.edu>
2022-06-17 17:24:59 -0500

    fetch github API data

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,13 @@
-import { useState } from 'react'
 import MaterialCard from './components/MaterialCard'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to my portfolio!</h1>
+        <h1>⚡️ Welcome to my portfolio! ⚡️</h1>
         <MaterialCard />
       </header>
-      <div>
-        <h1>Check out some of the tools that I use to be a successful programmer.👇</h1>
-        <div className='container'>
-          <div>
-            <h2>Programming Languages</h2>
-            <ul><a href="https://javascript.info/">JavaScript</a></ul>
-            <ul><a href="https://www.typescriptlang.org/">TypeScript</a></ul>
-          </div>
-          <div>
-            <h2>Libraries and Frameworks</h2>
-            <ul><a href="https://reactjs.org/">React</a></ul>
-          </div>
-          <div>
-            <h2>Coding Games</h2>
-            <ul><a href="https://flexboxfroggy.com/">Learn FlexBox with Flexfroggy</a></ul>
-            <ul><a href="https://vim-adventures.com/">Learn Vim with Vim-adventures</a></ul>
-          </div>
-        </div>
-      </div>
     </div>
   )
 }
--- src/components/MaterialCard.jsx
+++ src/components/MaterialCard.jsx
@@ -1,28 +1,48 @@
-import React from 'react'
-import Card from '@mui/material/Card'
+import { useState, useEffect } from 'react';
+import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
-import image from '../img/IMG_4154.jpeg';
 
-function MaterialCard() {
+export const MaterialCard = () => {
+	const [data, setData] = useState({});
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		setLoading(true);
+		fetch('https://api.github.com/users/hectorrami')
+		.then(response => response.json())
+		.then(data => setData(data))
+		.then(() => setLoading(false))
+		.catch(err => setError(err));
+	}, [])
+
+	const {name, bio, avatar_url, followers, following, location} = data;
+
+	if(loading) return <h1>Loading...</h1>
+
 	return (
 		<Card sx={{ maxWidth: 345}}>
 		<CardActionArea>
 			<CardMedia
 				component="img"
 				height="300"
-				image={image}
+				image={avatar_url}
 			/>
 			<CardContent>
 				<Typography gutterBottom variant="h5" component="div">
-					Hector Ramirez
+					{name}
+				</Typography>
+				<Typography gutterBottom variant="h7" component="div">
+					{location}
+				</Typography>
+				<Typography variant="body2" component="div">
+					{bio}
 				</Typography>
 				<Typography variant="body2" color="text.secondary">
-					Hi, I'm a software developer from Houston, Texas.I graduated from the 
-					University of Houston with a degree in Computer Science and currently work 
-					as a UI developer.
+					{`Follower: ${followers}, Following: ${following}`}
 				</Typography>
 			</CardContent>
 		</CardActionArea>
