af13edc6cc5139747f14813457b3df765be167ba
hectorrami <hramirez4@uh.edu>
2022-06-21 17:41:48 -0500

    Add eslint. Apply linter. Create navbar and skills components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import MaterialCard from './components/MaterialCard'
-import './App.css'
+import React from 'react';
+import NavBar from './components/NavBar';
+import MaterialCard from './components/MaterialCard';
+import Skills from './components/Skills';
+import './App.css';
+import logo from './logo.svg';
+
+const skillList = [{ name: 'React', image: logo }, { name: 'JavaScript', image: logo }, { name: 'TypeScript', image: logo }];
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
         <h1>⚡️ Welcome to my portfolio! ⚡️</h1>
         <MaterialCard />
       </header>
+      <h1>SKILLS</h1>
+      <div className="container-skills">
+        {
+        skillList.map((skill) => <Skills skill={skill.name} image={skill.image} />)
+      }
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/MaterialCard.jsx
+++ src/components/MaterialCard.jsx
@@ -1,53 +1,55 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 
-export const MaterialCard = () => {
-	const [data, setData] = useState({});
-	const [loading, setLoading] = useState(false);
-	const [error, setError] = useState(null);
+function MaterialCard() {
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-	useEffect(() => {
-		setLoading(true);
-		fetch('https://api.github.com/users/hectorrami')
-		.then(response => response.json())
-		.then(data => setData(data))
-		.then(() => setLoading(false))
-		.catch(err => setError(err));
-	}, [])
+  useEffect(() => {
+    setLoading(true);
+    fetch('https://api.github.com/users/hectorrami')
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .then(() => setLoading(false))
+      .catch((err) => setError(err));
+  }, []);
 
-	const {name, bio, avatar_url, followers, following, location} = data;
+  const {
+    name, bio, avatar_url, followers, following, location,
+  } = data;
 
-	if(loading) return <h1>Loading...</h1>
+  if (loading) return <h1>Loading...</h1>;
 
-	return (
-		<Card sx={{ maxWidth: 345}}>
-		<CardActionArea>
-			<CardMedia
-				component="img"
-				height="300"
-				image={avatar_url}
-			/>
-			<CardContent>
-				<Typography gutterBottom variant="h5" component="div">
-					{name}
-				</Typography>
-				<Typography gutterBottom variant="h7" component="div">
-					{location}
-				</Typography>
-				<Typography variant="body2" component="div">
-					{bio}
-				</Typography>
-				<Typography variant="body2" color="text.secondary">
-					{`Follower: ${followers}, Following: ${following}`}
-				</Typography>
-			</CardContent>
-		</CardActionArea>
-	</Card>
-	)
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="300"
+          image={avatar_url}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {name}
+          </Typography>
+          <Typography gutterBottom variant="h7" component="div">
+            {location}
+          </Typography>
+          <Typography variant="body2" component="div">
+            {bio}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {`Follower: ${followers}, Following: ${following}`}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
 }
 
-export default MaterialCard
+export default MaterialCard;
