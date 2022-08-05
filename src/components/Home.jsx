4c440836049556f077f1511263454f076f1c85ca
hectorrami <hramirez4@uh.edu>
2022-08-05 12:06:58 -0500

    Add new project

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,9 +5,13 @@ import Header from './Header';
 import Footer from './Footer';
 import Experience from './Experience';
 import NetflixClone from '../img/netflixclone.png';
+import AmazonClone from '../img/amazonclone.png';
 
 function Home() {
-  const projects = [{ img: NetflixClone, title: 'Netflix Clone' }];
+  const projects = [
+    { img: NetflixClone, title: 'Netflix Clone' },
+    { img: AmazonClone, title: 'Amazon Clone' },
+  ];
   return (
     <div>
       <Nav />
