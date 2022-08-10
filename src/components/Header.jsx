d8fcaf1df6dbc59855c990a52bebcf554beb0b3d
hectorrami <hramirez4@uh.edu>
2022-08-10 14:06:58 -0500

    Minor styling changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,9 +12,14 @@ function Header() {
     >
       <div className="header_contents">
         <h1 className="header_title">
-          Hector <span className="text-gradient">Ramirez</span>
+          <span className="text-gradient">Hector</span> Ramirez
         </h1>
-        <h1 className="header_description">Welcome to my portfolio!</h1>
+        <p className="header_description">
+          Hi. I'm a software engineer from Houston, Tx.
+          <br /> I have professional experience with React, JavaScript,
+          TypeScript, Java, and many other programming languages and tools.
+          <br /> My goal is to continue learning and growing as a developer.
+        </p>
       </div>
       <div className="header--fadeBottom" />
     </header>
