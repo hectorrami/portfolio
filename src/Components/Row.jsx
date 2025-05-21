6c3322f9910e5f7350d840e1df1bfcf22c07e16f
hectorrami <hramirez4@uh.edu>
2025-05-20 20:27:53 -0500

    add projects section

--- src/Components/Row.jsx
+++ src/Components/Row.jsx
@@ -1,18 +1,32 @@
-import React, {useContext} from 'react';
-import {ThemeContext} from "../Providers/ThemeContext";
+import React, { useContext } from 'react';
+import { ThemeContext } from "../Providers/ThemeContext";
 import SkillsChart from "./SkillsChart";
 import global from '../Styles/global.css'
 import './Row.css';
 
 function Row({ projects }) {
-    const {theme} = useContext(ThemeContext);
+  const { theme } = useContext(ThemeContext);
 
+  console.log('here:')
   return (
-      <div className={`row ${theme} ${theme === 'light' ? 'row-light' : 'row-dark'}`}>
-          <div className="row_posters">
-            <SkillsChart />
+    <div className={`row ${theme} ${theme === 'light' ? 'row-light' : 'row-dark'}`}>
+      <h3 className="row-text">Projects</h3>
+      <div className="row_posters">
+        {projects && projects.map((project) => (
+          <div>
+            <img src={project.img} alt="project" className="row_poster" />
+            <h3 className="row-text">Netlix Clone</h3>
+            <div className="banner_buttons">
+              <a className={'banner_button'} href={project.githubLink}>Github</a>
+              <a className={'banner_button'} href={project.previewLink}>Demo</a>
+            </div>
           </div>
+        ))}
       </div>
+      <div className="row_posters">
+        <SkillsChart />
+      </div>
+    </div>
   );
 }
 
