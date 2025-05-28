c12c63beef50b760e4b7fd5cd6097b55fca68cd4
hectorrami <hramirez4@uh.edu>
2025-05-28 17:34:26 -0500

    move links to navbar and remove them from footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,29 +1,20 @@
-import React, {useContext} from 'react';
-import {ThemeContext} from "../Providers/ThemeContext";
-import {GitHub, LinkedIn} from "@mui/icons-material";
+import React, { useContext } from 'react';
+import { ThemeContext } from "../Providers/ThemeContext";
 import './Footer.css';
 
 function Footer() {
-  const {theme} = useContext(ThemeContext);
+    const { theme } = useContext(ThemeContext);
 
-  const copyright = () => {
-      const year = new Date().getFullYear();
-      return `© ${year} Hector Ramirez. All rights reserved.`;
-  }
+    const copyright = () => {
+        const year = new Date().getFullYear();
+        return `© ${year} Hector Ramirez. All rights reserved.`;
+    }
 
-  return (
-      <div className={`footer ${theme}`}>
-          <p className={'footer-text'}>{copyright()}</p>
-          <div className={'footer-link'}>
-              <a href={'https://github.com/hectorrami'} target={'_blank'}>
-                  <GitHub className={`${theme}`} />
-              </a>
-              <a href={'https://www.linkedin.com/in/hector-ramirez-14ab63185'} target={'_blank'}>
-                  <LinkedIn className={`${theme}`}/>
-              </a>
-          </div>
-      </div>
-  );
+    return (
+        <div className={`footer ${theme}`}>
+            <p className={'footer-text'}>{copyright()}</p>
+        </div>
+    );
 }
 
 export default Footer;
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { ThemeContext } from '../Providers/ThemeContext';
-import {Bedtime, WbSunny} from '@mui/icons-material';
-import {IconButton} from "@mui/material";
+import { Menu, Bedtime, WbSunny, GitHub, LinkedIn } from '@mui/icons-material';
+import { IconButton } from "@mui/material";
 import global from '../Styles/global.css'
 import './Nav.css';
 
 function Nav() {
-  const {theme, toggleTheme} = useContext(ThemeContext);
+  const { theme, toggleTheme } = useContext(ThemeContext);
   const [show, handleShow] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const transitionNavbar = () =>
     window.scrollY > 100 ? handleShow(true) : handleShow(false);
@@ -20,9 +21,29 @@ function Nav() {
 
   return (
     <div className={`nav ${theme}`}>
-        <IconButton onClick={toggleTheme}>
-          {theme === 'light' ? <Bedtime className={`${theme}`}/> : <WbSunny className={`${theme}`}/>}
-        </IconButton >
+      <div className="nav_contents">
+        <div className="nav_left">
+          {/* <IconButton>
+            <Menu className={`${theme}`} />
+          </IconButton> */}
+        </div>
+
+        <div className="nav_right">
+          <IconButton onClick={toggleTheme}>
+            {theme === 'light' ? <Bedtime className={`${theme}`} /> : <WbSunny className={`${theme}`} />}
+          </IconButton>
+          <IconButton>
+            <a href="https://github.com/hectorrami" target="_blank" rel="noopener noreferrer">
+              <GitHub className={`${theme}`} />
+            </a>
+          </IconButton>
+          <IconButton>
+            <a href={'https://www.linkedin.com/in/hector-ramirez-14ab63185'} target={'_blank'}>
+              <LinkedIn className={`${theme}`} />
+            </a>
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
