8b945239189a2e6593199f3b596838bfbea04252
hectorrami <hramirez4@uh.edu>
2025-06-12 17:28:11 -0500

    Update headshot img

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Avatar } from '@mui/material';
-import Me from '../img/me.png';
+import headshot from '../img/headshot.png';
 
 function Header() {
 
   return (
       <div className="pt-24 pb-10 flex flex-col items-center dark:bg-gray-900">
-        <Avatar className="avatar" alt="Hector Ramirez" src={Me} sx={{ width: 200, height: 200 }}
+        <Avatar className="avatar" alt="Hector Ramirez" src={headshot} sx={{ width: 200, height: 200 }}
         />
       </div>
   );
