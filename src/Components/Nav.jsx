b7d649f3528f2593d8e5ae7812dec282a89d7d1a
hectorrami <hramirez4@uh.edu>
2025-06-12 15:33:15 -0500

    update navbar

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Nav() {
   return (
-    <div className='bg-white shadow'>
+    <div className='fixed top-0 left-0 w-full bg-white shadow-lg z-50'>
       <div className='max-w-7xl mx-auto px-4 py-4 flex justify-between items-center'>
         <div class="text-xl font-bold text-gray-800">
           Hector Ramirez
