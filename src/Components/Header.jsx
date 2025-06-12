287a4c3863962a483d2b2ba629b39b9506767571
hectorrami <hramirez4@uh.edu>
2025-06-12 15:43:49 -0500

    update header section

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,19 +1,14 @@
-import React, { useContext } from 'react';
-import { ThemeContext } from "../Providers/ThemeContext";
+import React from 'react';
 import { Avatar } from '@mui/material';
 import Me from '../img/me.png';
-import './Header.css';
 
 function Header() {
-  const { theme } = useContext(ThemeContext);
 
   return (
-    <header className={`header ${theme}`}>
-      <div className="header_contents">
+      <div className="pt-24 pb-10 flex flex-col items-center">
         <Avatar className="avatar" alt="Hector Ramirez" src={Me} sx={{ width: 200, height: 200 }}
         />
       </div>
-    </header>
   );
 }
 
