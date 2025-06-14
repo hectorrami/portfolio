822c07befb77f4c9d67b0299a74b74ad482c9be7
hectorrami <hramirez4@uh.edu>
2025-06-14 13:49:17 -0500

    remove unused theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import Home from './Components/Home';
-import { ThemeProvider } from './Providers/ThemeContext';
 
 function App() {
   return (
-    <ThemeProvider>
-      <Home />
-    </ThemeProvider>
+    <Home />
   );
 }
 
