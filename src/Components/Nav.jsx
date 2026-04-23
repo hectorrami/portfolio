fd060ea1560498e6c0a1f05d2654620cdf4e4687
hectorrami <hectorramirez096@gmail.com>
2026-04-23 15:46:14 -0500

    remove dash

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -7,7 +7,7 @@ function Nav() {
       <div className="fixed top-0 left-0 w-full bg-white dark:bg-gray-900 shadow-lg z-50">
         <div className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
           <div className="font-mono font-bold text-gray-800 dark:text-white text-lg">
-            <span className="text-green-500 dark:text-green-400">$</span> hector-ramirez.xyz
+            <span className="text-green-500 dark:text-green-400">$</span> hectorramirez.xyz
           </div>
           <DarkModeToggle />
         </div>
